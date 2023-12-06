b2dd3feea6534414a9317de623162aa808137824
njugunamwangi <njugunamwangi.n@gmail.com>
2023-12-06 23:42:23 +0300

    Role-Permission relationship

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Permission extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function roles(): BelongsTo {
+        return $this->belongsTo(Role::class);
+    }
 }
